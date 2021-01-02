859fc09086c8b1f05b61e654e9a6ec7a40d62682
Hoàng Đại <daihoangnam3it@gmail.com>
2021-01-02 22:29:00 +0700

    Update handle note

--- components/NoteItem.js
+++ components/NoteItem.js
@@ -1,24 +1,189 @@
-import React from 'react'
-import { StyleSheet, Text, View,Image } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  Button,
+  FlatList,
+  Platform,
+  Alert,
+  Modal,
+  TouchableHighlight,
+  TextInput,
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Picker } from '@react-native-picker/picker';
+import axios from 'axios';
+import URL from '../index'
+const NoteItem = ({item,navigation,listCate,listPrio,listStatus,handelDelete}) => {
+  //Modal
+  const [modalVisible, setModalVisible] = useState(false);
+  //Thuộc tính
+  const [term, setTerm] = useState(item.name);
+  const [status, setStatus] = useState(item.status)
+  const [cate, setCate] = useState(item.category)
+  const [prio, setPrio] = useState(item.priority)
 
-const NoteItem = ({item,navigation}) => {
-  // console.log('item',item);
-  const date=new Date(item.plandate);
-  const month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  
+ //DatePicker
+   //DatePicker
+   const [date, setDate] = useState(new Date(item.plandate));
+   const [mode, setMode] = useState('date');
+   const [show, setShow] = useState(false);
+ 
+ const month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+ const onChange = (event, selectedDate) => {
+   const currentDate = selectedDate || date;
+   setShow(Platform.OS === 'ios');
+   setDate(currentDate);
+ };
+ const showMode = (currentMode) => {
+   setShow(true);
+   setMode(currentMode);
+ };
+ const showDatepicker = () => {
+   showMode('date');
+ };
+ //Xử lý update
+ const handleUpdate=async ()=>{
+   const newNote={
+     name:term,
+     status:status,
+     priority:prio,
+     category:cate,
+     plandate:date,
+   }
+   const res=await axios.post(`${URL}/mobile-note/edit/${item._id}`,newNote);
+   Alert.alert(res.data.msg)
+   setModalVisible(!modalVisible)
+ }
+ //Xử lý status
+  const handleStatus = (value) => {
+    const newValue = value;
+    setStatus(newValue)
+  }
+  //Xử lý prio
+ const handlePrio = (value) => {
+    const newValue = value;
+    setPrio(newValue)
+  }
+  //Xử lý thể loại
+  const handleCate = (value) => {
+    const newValue = value;
+    setCate(newValue)
+  }
+  //Xử lý đóng form
+  const handleCloseModal=()=>{
+    setModalVisible(!modalVisible);
+    setCate(item.category);
+    setPrio(item.priority);
+    setStatus(item.status);
+    setDate(new Date(item.plandate));
+    setTerm(item.name)
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.box} >
     {/* <Image
       source={{uri:item.avatar}}
       style={styles.img}
     /> */}
-      <Text>Name: {item.name}</Text>
-      <Text>Category: {item.category}</Text>
-      <Text>Priority: {item.priority}</Text>
-      <Text>Status: {item.status}</Text>
+      <TouchableOpacity onPress={()=>setModalVisible(true)}>
+      <Text>Name: {term}</Text>
+      <Text>Category: {cate}</Text>
+      <Text>Priority: {prio}</Text>
+      <Text>Status: {status}</Text>
       <Text>PlanDate: {date.getDate()}/{month[date.getMonth()]}/{date.getFullYear()}</Text>
       <Text>CreateAt: {item.createAt}</Text>
 
-     
+      </TouchableOpacity>
+
+      <Modal
+        transparent={true}
+        animationType='slide'
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TouchableOpacity
+              style={styles.close}
+              onPress={() => handleCloseModal()}
+            >
+              <Text style={{ fontSize: 40 }}>X</Text>
+            </TouchableOpacity>
+            <TextInput
+              placeholder="Nhập tên"
+              style={styles.input}
+              value={term}
+              onChangeText={(text) => setTerm(text)}
+            />
+            <View>
+              <Picker selectedValue={cate} onValueChange={handleCate.bind()} style={{ height: 50, width: 200 }}>
+                <Picker.Item label={cate} value={cate} />
+                {listCate.map((el,index)=> {
+                  return <Picker.Item label={el.name} value={el.name} key={index}/>
+                })}
+              </Picker>
+              <Picker selectedValue={prio} onValueChange={handlePrio.bind()} style={{ height: 50, width: 200 }}>
+                <Picker.Item label={prio} value={prio} />
+                {listPrio.map(el => {
+                  return <Picker.Item label={el.name} value={el.name} />
+                })}
+              </Picker>
+              <Picker selectedValue={status} onValueChange={handleStatus.bind()} style={{ height: 50, width: 200 }}>
+                <Picker.Item label={status} value={status} />
+                {listStatus.map(el => {
+                  return <Picker.Item label={el.name} value={el.name} />
+                })}
+              </Picker>
+            </View>
+            <View>
+              <View style={styles.date}>
+                <View style={styles.textdate}>
+                <Text>
+                  {date.getUTCDate()}/{month[date.getUTCMonth()]}/
+              {date.getFullYear()}
+
+                </Text>
+                </View>
+                <Text onPress={showDatepicker} style={styles.btn} >...</Text>
+              </View>
+
+              {show && (
+                <DateTimePicker
+                  testID='dateTimePicker'
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  display='default'
+                  onChange={onChange}
+                />
+              )}
+
+            </View>
+            <View style={{
+              width:200,
+              flexDirection:"row",
+              justifyContent:'space-between',
+              
+            }}>
+            <TouchableHighlight
+              style={styles.openButton}
+              onPress={() => handleUpdate()}
+            >
+              <Text>Cập nhật</Text>
+            </TouchableHighlight>
+                <TouchableHighlight onPress={()=>handelDelete(item._id)} style={styles.openButton}>
+                  <Text>Xoá</Text>
+                </TouchableHighlight>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -26,12 +191,114 @@ const NoteItem = ({item,navigation}) => {
 export default NoteItem
 
 const styles = StyleSheet.create({
-  container:{
-    marginBottom:5
-  },  
-  img:{
-    width:50,
-    height:50,
-    borderRadius:50,
-  }
-})
+  box:{
+    marginBottom:2,
+    paddingBottom:3,
+    borderBottomColor:"#000",
+    borderBottomWidth:1
+  },
+  textdate: {
+    borderWidth: 2,
+    borderColor: "#000",
+    width: 130,
+    height: 25,
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems:"center"
+  },
+  btn: {
+    backgroundColor: "#fff",
+    marginRight: 2,
+    width: 40,
+    height: 25,
+    textAlign: "center",
+    fontSize: 20,
+    borderWidth: 2,
+    borderColor: "#000",
+  },
+  date: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 200
+  },
+  input: {
+    width: 200,
+    height: 35,
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    marginBottom: 2,
+    padding: 2,
+    fontSize: 20,
+  },
+  close: {
+    position: 'absolute',
+    top: 5,
+    right: 15,
+
+    justifyContent: 'flex-end',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'transparent',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 50,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  scroll: {
+    width: '100%',
+    padding: 16,
+    paddingBottom: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  text: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  add: {
+    marginRight: 10,
+    fontSize: 50,
+    borderWidth: 2,
+    height: 20,
+    width: 20,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
--- screens/main.js
+++ screens/main.js
@@ -14,29 +14,34 @@ import {
   TextInput,
 } from 'react-native';
 import { AuthContext } from '../App';
-import { NavigationContainer,useFocusEffect } from '@react-navigation/native';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
 import URL from '../index'
 import Item from '../components/NoteItem';
 const Main = ({ navigation, name }) => {
+  //Xác thực
   const { authContext, user } = React.useContext(AuthContext);
+  //Modal
   const [modalVisible, setModalVisible] = useState(false);
+  //DatePicker
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
   const [data, setData] = useState([]);
-  const [term, setTerm] = useState('');
-  
-  //PICKER
-  const [statusList,setStatusList]=useState([]);
-  const [status,setStatus]=useState('')
-  const [cateList,setCateList]=useState([]);
-  const [cate,setCate]=useState('')
-  const [prioList,setPrioList]=useState([]);
-  const [prio,setPrio]=useState('')
 
+
+  //Thuộc tính
+  const [term, setTerm] = useState('');
+  const [statusList, setStatusList] = useState([]);
+  const [status, setStatus] = useState('')
+  const [cateList, setCateList] = useState([]);
+  const [cate, setCate] = useState('')
+  const [prioList, setPrioList] = useState([]);
+  const [prio, setPrio] = useState('')
+
+  //DatePicker
   const month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -50,22 +55,69 @@ const Main = ({ navigation, name }) => {
   const showDatepicker = () => {
     showMode('date');
   };
-  const getData=async ()=>{
-    const res=await axios.get(`${URL}/mobile-note/get/${user}`);
+
+
+  //Xử lý note
+  const getData = async () => {
+    const res = await axios.get(`${URL}/mobile-note/get/${user}`);
     console.log(res.data);
     setData(res.data)
   }
-  const getStatus=async ()=>{
-    const cate=await axios.get(`${URL}/mobile-stt/get/${user}`);
+  const handleCreate = async () => {
+    const newNote = {
+      name: term,
+      idUser: user,
+      status: status,
+      category: cate,
+      priority: prio,
+      plandate: date
+    }
+    // console.log(newNote);
+    const res = await axios.post(`${URL}/mobile-note/create`, newNote)
+    let newData = [...data, res.data]
+    setData(newData)
+    setModalVisible(!modalVisible);
+    setCate('');
+    setTerm('')
+    setDate(new Date())
+    setPrio('')
+    setStatus('')
+  }
+  //Xử lý xoá
+  const handelDelete=async(id)=>{
+    const res=await axios.get(`${URL}/mobile-note/delete/${id}`);
+    let newData=data.map(el=>el);
+    newData=newData.filter(el=>el._id!==id)
+    setData(newData);
+    Alert.alert(res.data.msg)
+  }
+
+  //Xử lý status
+  const getStatus = async () => {
+    const cate = await axios.get(`${URL}/mobile-stt/get/${user}`);
     setStatusList(cate.data)
   }
-  const getCate=async ()=>{
-    const cate=await axios.get(`${URL}/mobile-cate/get/${user}`);
+  const handleStatus = (value) => {
+    const newValue = value;
+    setStatus(newValue)
+  }
+  //Xử lý thể loại
+  const getCate = async () => {
+    const cate = await axios.get(`${URL}/mobile-cate/get/${user}`);
     setCateList(cate.data)
   }
-  const getPrio=async ()=>{
-    const cate=await axios.get(`${URL}/mobile-prio/get/${user}`);
+  const handleCate = (value) => {
+    const newValue = value;
+    setCate(newValue)
+  }
+  //Xử lý prio
+  const getPrio = async () => {
+    const cate = await axios.get(`${URL}/mobile-prio/get/${user}`);
     setPrioList(cate.data)
+  }
+  const handlePrio = (value) => {
+    const newValue = value;
+    setPrio(newValue)
   }
   useFocusEffect(
     React.useCallback(() => {
@@ -75,253 +127,228 @@ const Main = ({ navigation, name }) => {
       getPrio();
     }, [user]));
 
-  const handleStatus=(value)=>{
-    const newValue=value;
-    setStatus(newValue)
-  }
-  const handleCate=(value)=>{
-    const newValue=value;
-    setCate(newValue)
-  }
-  const handlePrio=(value)=>{
-    const newValue=value;
-    setPrio(newValue)
-  }
-  const handleCreate=async()=>{
-    const newNote={
-      name:term,
-      idUser:user,
-      status:status,
-      category:cate,
-      priority:prio,
-      plandate:date
-    }
-    // console.log(newNote);
-    const res=await axios.post(`${URL}/mobile-note/create`,newNote)
-    console.log(res);
-    let newData=[...data,res.data]
-    setData(newData)
-    setModalVisible(!modalVisible);
-    setCate('');
-    setTerm('')
-    setDate(new Date())
-    setPrio('')
-    setStatus('')
-  }
+
   return (
     <View style={styles.container}>
-    {/* <TouchableOpacity
+      {/* <TouchableOpacity
       style={{ alignItems: 'flex-end', margin: 16 }}
       onPress={() => navigation.openDrawer()}
     >
       <FontAwesome name='bars' size={32} color='black' />
     </TouchableOpacity> */}
 
-    <FlatList
-      data={data}
-      keyExtractor={(item) => `${item}`}
-      renderItem={({ item, index }) => {
-        return <Item item={item} navigation={navigation} />;
-      }}
-      contentContainerStyle={styles.scroll}
-    />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => `${item._id}`}
+        renderItem={({ item, index }) => {
+          return <Item
+            item={item}
+            navigation={navigation}
+            listCate={cateList}
+            listPrio={prioList}
+            listStatus={statusList}
+            handelDelete={handelDelete}
+          />;
+        }}
+        contentContainerStyle={styles.scroll}
+      />
 
-    <View
-      style={{
-        height: 50,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-      }}
-    >
-      <TouchableOpacity
-        style={styles.add}
-        onPress={() => setModalVisible(true)}
+      <View
+        style={{
+          height: 50,
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}
       >
-        <Text>+</Text>
-      </TouchableOpacity>
-    </View>
-    <Modal
-      transparent={true}
-      animationType='slide'
-      visible={modalVisible}
-      onRequestClose={() => {
-        Alert.alert('Modal has been closed.');
-      }}
-    >
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-          <TouchableOpacity
-            style={styles.close}
-            onPress={() => {
-              setModalVisible(!modalVisible);
-            }}
-          >
-            <Text style={{ fontSize: 40 }}>X</Text>
-          </TouchableOpacity>
-          <TextInput
-          placeholder="Nhập tên"
-            style={styles.input}
-            value={term}
-            onChangeText={(text) => setTerm(text)}
-          />
-          <View>
-            <Picker selectedValue={cate} onValueChange={handleCate.bind()}  style={{height: 50, width: 200}}>
-                <Picker.Item label="Chọn thể loại" value="0" />
-               {cateList.map(el=>{
-                 return <Picker.Item label={el.name} value={el.name}/>
-               })}
-            </Picker>
-            <Picker selectedValue={prio} onValueChange={handlePrio.bind()} style={{height: 50, width: 200}}>
-                <Picker.Item label="Chọn ưu tiên" value="0" />
-                {prioList.map(el=>{
-                 return <Picker.Item label={el.name} value={el.name}/>
-               })}
-            </Picker>
-            <Picker selectedValue={status} onValueChange={handleStatus.bind()} style={{height: 50, width: 200}}>
-                <Picker.Item label="Chọn trạng thái" value="0" />
-                {statusList.map(el=>{
-                  return <Picker.Item label={el.name} value={el.name}/>
-                })}
-            </Picker>
-          </View>
-          <View>
-            <View style={styles.date}>
-            <Text style={styles.textdate}>
-              {date.getUTCDate()}/{month[date.getUTCMonth()]}/
-              {date.getFullYear()}
-            </Text>
-              <Text onPress={showDatepicker} style={styles.btn} >...</Text>
-            </View>
-
-            {show && (
-              <DateTimePicker
-                testID='dateTimePicker'
-                value={date}
-                mode={mode}
-                is24Hour={true}
-                display='default'
-                onChange={onChange}
-              />
-            )}
-           
-          </View>
-          <TouchableHighlight
-            style={styles.openButton}
-            onPress={() => handleCreate()}
-          >
-            <Text>Xác nhận</Text>
-          </TouchableHighlight>
-        </View>
+        <TouchableOpacity
+          style={styles.add}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text>+</Text>
+        </TouchableOpacity>
       </View>
-    </Modal>
-  </View>
-);
+      <Modal
+        transparent={true}
+        animationType='slide'
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TouchableOpacity
+              style={styles.close}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={{ fontSize: 40 }}>X</Text>
+            </TouchableOpacity>
+            <TextInput
+              placeholder="Nhập tên"
+              style={styles.input}
+              value={term}
+              onChangeText={(text) => setTerm(text)}
+            />
+            <View>
+              <Picker selectedValue={cate} onValueChange={handleCate.bind()} style={{ height: 50, width: 200 }}>
+                <Picker.Item label="Chọn thể loại" value="0" />
+                {cateList.map(el => {
+                  return <Picker.Item label={el.name} value={el.name} />
+                })}
+              </Picker>
+              <Picker selectedValue={prio} onValueChange={handlePrio.bind()} style={{ height: 50, width: 200 }}>
+                <Picker.Item label="Chọn ưu tiên" value="0" />
+                {prioList.map(el => {
+                  return <Picker.Item label={el.name} value={el.name} />
+                })}
+              </Picker>
+              <Picker selectedValue={status} onValueChange={handleStatus.bind()} style={{ height: 50, width: 200 }}>
+                <Picker.Item label="Chọn trạng thái" value="0" />
+                {statusList.map(el => {
+                  return <Picker.Item label={el.name} value={el.name} />
+                })}
+              </Picker>
+            </View>
+            <View>
+              <View style={styles.date}>
+                <Text style={styles.textdate}>
+                  {date.getUTCDate()}/{month[date.getUTCMonth()]}/
+              {date.getFullYear()}
+                </Text>
+                <Text onPress={showDatepicker} style={styles.btn} >...</Text>
+              </View>
+
+              {show && (
+                <DateTimePicker
+                  testID='dateTimePicker'
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  display='default'
+                  onChange={onChange}
+                />
+              )}
+
+            </View>
+            <TouchableHighlight
+              style={styles.openButton}
+              onPress={() => handleCreate()}
+            >
+              <Text>Xác nhận</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
 };
 
 export default Main;
 
 const styles = StyleSheet.create({
-textdate:{
-  borderWidth:2,
-  borderColor:"#000",
-  width:130,
-  height:25,
-  justifyContent:"center",
-  textAlign:"center",
-},
-btn:{
-  backgroundColor:"#fff",
-  marginRight:2,
-  width:40,
-  height:25,
-  textAlign:"center",
-  fontSize:20,
-  borderWidth:2,
-  borderColor:"#000",
-},
-date:{
-  flexDirection:"row",
-  justifyContent:"space-between",
-  alignItems:"center",
-  marginBottom:20,
-  width:200
-},
-input: {
-  width: 200,
-  height: 35,
-  borderColor: 'gray',
-  borderBottomWidth: 1,
-  marginBottom: 2,
-  padding: 2,
-  fontSize: 20,
-},
-close: {
-  position: 'absolute',
-  top: 5,
-  right: 15,
-
-  justifyContent: 'flex-end',
-},
-centeredView: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginTop: 22,
-  backgroundColor: 'transparent',
-},
-modalView: {
-  margin: 20,
-  backgroundColor: '#fff',
-  borderRadius: 20,
-  padding: 50,
-  alignItems: 'center',
-  shadowColor: '#000',
-  shadowOffset: {
-    width: 0,
-    height: 2,
+  textdate: {
+    borderWidth: 2,
+    borderColor: "#000",
+    width: 130,
+    height: 25,
+    justifyContent: "center",
+    textAlign: "center",
   },
-  shadowOpacity: 0.25,
-  shadowRadius: 3.84,
-  elevation: 5,
-},
-openButton: {
-  backgroundColor: '#F194FF',
-  borderRadius: 20,
-  padding: 10,
-  elevation: 2,
-},
-textStyle: {
-  color: 'white',
-  fontWeight: 'bold',
-  textAlign: 'center',
-},
-modalText: {
-  marginBottom: 15,
-  textAlign: 'center',
-},
-scroll: {
-  width: '100%',
-  padding: 16,
-  paddingBottom: 20,
-},
-container: {
-  flex: 1,
-  backgroundColor: '#fff',
-},
-text: {
-  color: '#000',
-  fontSize: 20,
-  fontWeight: '500',
-},
-add: {
-  marginRight: 10,
-  fontSize: 50,
-  borderWidth: 2,
-  height: 20,
-  width: 20,
-  borderRadius: 50,
-  alignItems: 'center',
-  justifyContent: 'center',
-},
+  btn: {
+    backgroundColor: "#fff",
+    marginRight: 2,
+    width: 40,
+    height: 25,
+    textAlign: "center",
+    fontSize: 20,
+    borderWidth: 2,
+    borderColor: "#000",
+  },
+  date: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 200
+  },
+  input: {
+    width: 200,
+    height: 35,
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    marginBottom: 2,
+    padding: 2,
+    fontSize: 20,
+  },
+  close: {
+    position: 'absolute',
+    top: 5,
+    right: 15,
+
+    justifyContent: 'flex-end',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'transparent',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 50,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  scroll: {
+    width: '100%',
+    padding: 16,
+    paddingBottom: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  text: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  add: {
+    marginRight: 10,
+    fontSize: 50,
+    borderWidth: 2,
+    height: 20,
+    width: 20,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
